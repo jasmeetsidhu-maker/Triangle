73b802f1d30c660dbeab7f33b9fd876ac32f1edd
Jsidhu0642 <Jsidhu0642@conestogac.on.ca>
2020-02-19 18:27:30 -0500

    all don

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Triangle
 {
-    class Triangle
+    class Triangle 
     {
         static void Main(string[] args)
         {
@@ -25,25 +25,29 @@ namespace Triangle
                     {
                         case 1:
                             Console.WriteLine("Please Enter Three Sides Triangle\n");
-
+                            Console.WriteLine("Check it Form a triangle or not");
+                            Console.WriteLine("triangle is scalene, isosceles or equilateral\n");
                             int s1 = 0;
                             int s2 = 0;
                             int s3 = 0;
 
-                            Console.Write("Enter Value of Side 1 for Triangle:\n ");
+                            Console.Write("Enter Value of Side 1 for Triangle: ");
                             s1 = Convert.ToInt32(Console.ReadLine());
 
-                            Console.Write("Enter Value of Side 2 for Triangle:\n ");
+                            Console.Write("Enter Value of Side 2 for Triangle:");
                             s2 = Convert.ToInt32(Console.ReadLine());
 
-                            Console.Write("Enter Value of Side 3 for Triangle:\n ");
+                            Console.Write("Enter Value of Side 3 for Triangle: ");
                             s3 = Convert.ToInt32(Console.ReadLine());
-                            
+
+
+                            string res = TriangleSolver.Analyze(s1, s2, s3);
+                            Console.WriteLine(res);
+
                             break;
 
                         case 2:
                             Console.WriteLine("Thank you.\n");
-                            Console.ReadLine();
                             System.Environment.Exit(0);
                             rpt = false;
                             break;
---
+++ TriangleSolver.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Triangle
+{
+    public static class TriangleSolver
+    {
+        public static string Analyze (int angle1, int angle2, int angle3 )
+        {
+            string trianlge = string.Empty;
+
+
+            if ((angle1 != angle2 || (angle2 != angle3) | (angle3 != angle1)))
+                {
+                trianlge = "Triangle is Scalene \n ";
+            }
+
+            else if (angle1==angle2 | angle2==angle3 | angle3==angle1)
+            {
+                trianlge = "Triangle is Isosceles \n";
+            }
+
+            else if ((angle1==angle2)&& (angle2 == angle3) & (angle3 == angle1))
+            {
+                trianlge = "Triangle is Equilateral \n ";
+            }
+            else
+            {
+                trianlge = " It's not a Triangle";
+            }
+            return trianlge;
+
+        }
+
+
+
+
+
+
+        }
+
+    
+}
